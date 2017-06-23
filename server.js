97dd8692d4e991dc81e777d83902273c7b1a9735
cloudle <lehaoson@gmail.com>
2017-06-23 21:46:21 +0700

    Refactor server run modes - much cleaner..

--- server.js
+++ server.js
@@ -1,68 +1,25 @@
 const express = require('express');
-const next = require('next');
-const moduleAlias = require('module-alias');
+const serverUtils = require('./serverUtils');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 2017;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const useNextSSR = process.env.USE_NEXT !== 'false';
 const server = express();
 
-moduleAlias.addAlias('react-native', 'react-native-web');
+if (dev) serverUtils.watchAndHotInject();
 
-// Hot reload server module by watch and inject changes
-if (dev) {
-	const chokidar = require('chokidar');
-	const watcher = chokidar.watch('./server');
+server.use(function (req, res, next) {
+	const router = dev ?
+		require('./server/router') : require('./.server/router');
 
-	watcher.on('ready', function() {
-		watcher.on('all', function() {
-			Object.keys(require.cache).forEach(function(id) {
-				if (/[\/\\]server[\/\\]/.test(id)) {
-					delete require.cache[id];
-				}
-			});
+	router(req, res, next);
+});
 
-			console.log("Server was hot updated..");
-		})
-	});
-}
-
-app.prepare()
-	.then(() => {
-		// Temporary fix for Bodymovin.. on server side
-		global.requestAnimationFrame = () => {};
-		global.document = {
-			createElement: () => ({ getContext: () => {} }),
-			createElementNS: () => ({ getContext: () => {} }),
-			getElementsByTagName: () => ({ getContext: () => {} }),
-		};
-		global.navigator = {
-			userAgent: 'user-agent',
-			isServer: true,
-		};
-
-		server.use(function (req, res, next) {
-			const router = dev ?
-				require('./server/router') : require('./.server/router');
-
-			router(req, res, next);
+serverUtils.conditionallyInjectNextSSR(server, dev, useNextSSR).then((server) => {
+	if (module === require.main) {
+		server.listen(port, (err) => {
+			if (err) throw err;
+			console.log(`Server is running under port ${port}`);
 		});
-
-		server.get('*', (req, res) => {
-			return handle(req, res);
-		});
-
-		if (module === require.main) {
-			server.listen(port, (err) => {
-				if (err) throw err;
-				console.log(`Server is running under port ${port}`);
-			});
-		}
-	})
-	.catch((ex) => {
-		console.error(ex.stack);
-		process.exit(1);
-	});
-
-module.exports = server;
+	}
+}).catch((ex) => { console.error(ex.stack); process.exit(1) });
--- server/router.js
+++ server/router.js
@@ -1,18 +1,80 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import Datastore from '@google-cloud/datastore';
 
 const router = express.Router();
+const datastore = Datastore();
 
 router.use('/api', cors(), bodyParser.json(), (req, res) => {
 	const { query, variables } = req.body,
 		clientId = req.get('clientId');
 
-	res.json({ message: 'hello world!' });
+	listMarkdowns()
+		.then(markdowns => console.log(markdowns))
+		.catch(error => console.log(error));
+
+	res.json({ message: 'Hello world!' });
 
 	// return execute(query, variables, { clientId, })
 	// 	.then(result => res.json(result))
 	// 	.catch(error => res.json(error));
 });
+
+function createMarkDown(content) {
+	const markdownKey = datastore.key('Markdown');
+	const entity = {
+		key: markdownKey,
+		data: [{
+			name: 'created',
+			value: new Date().toJSON(),
+		}, {
+			name: 'content',
+			value: content,
+			excludeFromIndexes: true,
+		}, {
+			name: 'published',
+			value: false,
+		}],
+	};
+
+	return datastore.save(entity)
+		.then(() => {
+			console.log(`Markdown ${markdownKey.id} created successfully.`);
+			return markdownKey;
+		});
+}
+
+function publishMarkdown(markdownId) {
+	const transaction = datastore.transaction();
+	const markdownKey = datastore.key(['Markdown', markdownId]);
+
+	return transaction.run()
+		.then(() => transaction.get(markdownKey))
+		.then((results) => {
+			const markdown = results[0];
+			markdown.publish = true;
+			transaction.save({
+				key: markdownKey,
+				data: markdown,
+			});
+			return transaction.commit();
+		}).then(() => {
+			console.log(`Markdown ${markdownId} updated successfully.`)
+		}).catch(() => transaction.rollback());
+}
+
+function deleteMarkdown(markdownId) {
+	const markdownKey = datastore.key(['Markdown', markdownId]);
+
+	return datastore.delete(markdownKey)
+		.then(() => console.log(`Markdown ${markdownId} deleted successfully.`));
+}
+
+function listMarkdowns() {
+	const query = datastore.createQuery('Markdown')
+		.order('created');
+	return datastore.runQuery(query).then(results => results[0]);
+}
 
 module.exports = router;
